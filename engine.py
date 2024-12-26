74b7f2e29b8f1c9b97e7ef0f2f01be6a814c9b19
WonkManGood <ezrajw@pm.me>
2024-12-26 00:38:48 -0500

    init

---
+++ engine.py
@@ -0,0 +1,67 @@
+from time import sleep
+from gpiozero import PWMLED
+from re import findall, compile
+
+expression = compile(r'.\d')
+def find(i):
+    return (findall(expression, i))[0]
+
+def engine_write(line, value):
+    try:
+        line = int(line)
+        value = str(value)
+    except: return _
+
+    with open('./engine_comms', 'r') as f:
+        lines = f.readlines()
+        lines[line] = ''
+        lines[line] = str('{ '+value+' }\n')
+        with open('./engine_comms', 'w') as f:
+            f.writelines(lines)
+
+def engine_read(line):
+    line = line - 1
+    with open('./engine_comms', 'r') as f:
+        return str(find(f.readlines()[line])).strip()
+
+
+while  engine_read(1) == '1':
+    # /// BREAK ONE
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK TWO
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK THREE
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK FOUR
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK FIVE
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK SIX
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK SEVEN
+    ...
+    
+    sleep(0.125)
+
+    # /// BREAK EIGHT
+    ...
+    
+    sleep(0.125)
---
+++ testing.py
@@ -0,0 +1,3 @@
+from engine import engine
+e = engine()
+e.change_value(1, 0)
